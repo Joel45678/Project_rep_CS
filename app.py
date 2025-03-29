77de48b1fcbd704d6d9211ef4823aac421f236ac
Joel45678 <145365520+Joel45678@users.noreply.github.com>
2025-03-29 18:12:16 +0100

    titels

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ st.write("Everything conected") #Test if it works properly
 #List of all eating behaviors
 allergies = ["lactose", "x", "y"]
 diet = ["vegan", "vegetarian", "Pescetarian"]
+excluded_ingredients = ["vegan", "vegetarian", "Pescetarian"]
+
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Allergies")
-    st.selectbox("Allergies", allergies, key="allergies")
+    st.selectbox(allergies, key="allergies")
 
 with col2:
     st.header("Diet")
-    st.selectbox("Diet", diet, key="Diet")
+    st.selectbox(diet, key="Diet")
 
 with col3:
     st.header("Excluded ingredients")
-    st.text_input("Exclude ingredients", key="Excluded-ingredients")
+    st.selectbox(excluded_ingredients, key="Excluded-ingredients")
