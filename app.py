db8ce6f74e1e3963cafabbe9b63bf02361f3a4ea
Joel45678 <145365520+Joel45678@users.noreply.github.com>
2025-03-29 18:09:40 +0100

    rows

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #link to the app: https://projectrepcs-kgxuhxcx6ux2eygqo8g8ey.streamlit.app/
 
 import streamlit as st
-st.write("Everything conected - change") #Test if it works properly
+st.write("Everything conected") #Test if it works properly
 
 #List of all eating behaviors
 allergies = ["lactose", "x", "y"]
 diet = ["vegan", "vegetarian", "Pescetarian"]
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.header("Allergies")
+    st.selectbox("Allergies", allergies, key="allergies")
 
-st.selectbox("Allergies", allergies, key="allergies")
-st.selectbox("Diet", diet, key="Diet")
-st.text_input("Exclude ingredients", key="Excluded-ingredients")
+with col1:
+    st.header("Diet")
+    st.selectbox("Diet", diet, key="Diet")
+
+with col1:
+    st.header("Excluded ingredients")
+    st.text_input("Exclude ingredients", key="Excluded-ingredients")
