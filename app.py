c868ee4b44e6ee6956bc187348ad6f83af0f408e
Joel45678 <145365520+Joel45678@users.noreply.github.com>
2025-03-29 18:05:49 +0100

    Inputs

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 #link to the app: https://projectrepcs-kgxuhxcx6ux2eygqo8g8ey.streamlit.app/
-import streamlit as st
 
-st.write("Everything conected")
-#Test if it works properly
+import streamlit as st
+st.write("Everything conected") #Test if it works properly
+
+#List of all eating behaviors
+allergies = ["lactose", "x", "y"]
+diet = ["vegan", "vegetarian", "Pescetarian"]
+
+
+st.selectbox("Allergies", allergies, key="allergies")
+st.selectbox("Diet", diet, key="Diet")
+st.text_input("Exclude ingredients", key="Excluded-ingredients")
