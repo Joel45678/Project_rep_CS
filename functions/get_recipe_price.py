947c4ac703ebd9b6317f6c7395de9956de4aa40e
RickyKrt <rickykurt07@yahoo.com>
2025-04-08 16:26:36 +0200

    Added recipe details and images

--- functions/get_recipe_price.py
+++ functions/get_recipe_price.py
@@ -16,3 +16,17 @@ def get_recipe_price(API_KEY,recipe_id):
     #calculate total price
     total_price = price_per_serving * servings
     return total_price
+
+def get_recipe_details(API_KEY, recipe_id):
+    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
+    params = {"apiKey": API_KEY}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    title = data.get("title", "No title")
+    image = data.get("image", "")
+    instructions = data.get("instructions", "No instructions provided.")
+
+    return title, image, instructions
+
