13a2238a2760a2657d678e0b24871209bc5eab6d
Joel45678 <145365520+Joel45678@users.noreply.github.com>
2025-05-10 22:12:52 +0200

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -197,7 +197,7 @@ def regenerate_one(id_regenerate):
     }
 
 
-
+# ---- trigger forecasting model when dropdown selection changes --- #
 def run_forecast():
     csv_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'training_data_nutrition.csv')
     csv_path = os.path.abspath(csv_path)
@@ -218,7 +218,7 @@ def run_forecast():
 # Sidebar configuration inputs
 
 with st.sidebar:
-    #st.image("src/assets/01_Logo.png", width=150)
+    st.image("src/assets/01_Logo.png", width=150)
 
     st.number_input(
         label="Number of recipes",
@@ -236,28 +236,16 @@ with st.sidebar:
 
     st.button("Generate Meal Plan", on_click=generate_plan)
 
-    # Run forecast model
-    if st.button("ML Forecast anzeigen"):
-        csv_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'training_data_nutrition.csv')
-        csv_path = os.path.abspath(csv_path)
-
-        pred_cost = forecast_user_constraints(
-            csv_path,
-            diet=st.session_state.diet if st.session_state.diet != "none" else None,
-            intolerance=st.session_state.intolerances if st.session_state.intolerances != "none" else None,
-            excluded_ingredient=st.session_state.excluded_ingredients if st.session_state.excluded_ingredients != "none" else None
-        )
-
-        st.markdown(f"**Vorhergesagte Kosten basierend auf deinen Angaben:** {pred_cost:.2f} €")
+    # Show forecast directly in the sidebar, if available
+    if "forecast_avg" in st.session_state:
+        st.markdown(f"**Estimated price per serving:** {st.session_state.forecast_avg:.2f} €")
 
 
+    
 
 # Main content: Recipe display and charts
 col1f =st.columns(1)[0]
 with col1f:
-    # Show forecast
-    if "forecast_avg" in st.session_state:
-        st.markdown(f"**Estimated price per serving:** {st.session_state.forecast_avg:.2f} €")
 
     titles_placeholder = st.empty()  # placeholder for recipes
     price_placeholder = st.empty()   # placeholder for price
